f35e6cab84ff6fdbbb89ba56fafda91e7ddc23d6
Matheus H. Gois <matheus.hengo@gmail.com>
2021-06-22 18:37:18 -0300

    Exercise 2 done

--- src/TrafficSignal.jsx
+++ src/TrafficSignal.jsx
@@ -1,39 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import CarsContext from './context/CarsContext';
 import redSignal from './images/redSignal.jpeg';
 import yellowSignal from './images/yellowSignal.jpeg';
 import greenSignal from './images/greenSignal.jpeg';
 
-const renderSignal = (signalColor) => {
-  if (signalColor === 'red') return redSignal;
-  if (signalColor === 'yellow') return yellowSignal;
-  if (signalColor === 'green') return greenSignal;
-  return null;
-};
+// class TrafficSignal extends Component {
+//   render() {
+//     const { color } = this.context.signal;
+//     const { changeSignal } = this.context;
+//     return (
+//       <div>
+//         <div className="button-container">
+//           <button onClick={() => changeSignal('red')} type="button">
+//             Red
+//           </button>
+//           <button onClick={() => changeSignal('yellow')} type="button">
+//             Yellow
+//           </button>
+//           <button onClick={() => changeSignal('green')} type="button">
+//             Green
+//           </button>
+//         </div>
+//         <img className="signal" src={renderSignal(color)} alt="" />
+//       </div>
+//     );
+//   }
+// };
 
-class TrafficSignal extends Component {
-  render() {
-    const { color } = this.context.signal;
-    const { changeSignal } = this.context;
-    return (
-      <div>
-        <div className="button-container">
-          <button onClick={() => changeSignal('red')} type="button">
-            Red
-          </button>
-          <button onClick={() => changeSignal('yellow')} type="button">
-            Yellow
-          </button>
-          <button onClick={() => changeSignal('green')} type="button">
-            Green
-          </button>
-        </div>
-        <img className="signal" src={renderSignal(color)} alt="" />
+
+// TrafficSignal.contextType = CarsContext;
+
+function TrafficSignal(){
+  const { changeSignal, signal: { color } } = useContext(CarsContext);
+  
+  const renderSignal = (signalColor) => {
+    if (signalColor === 'red') return redSignal;
+    if (signalColor === 'yellow') return yellowSignal;
+    if (signalColor === 'green') return greenSignal;
+    return null;
+  };
+
+  return (
+    <div>
+      <div className="button-container">
+        <button onClick={() => changeSignal('red')} type="button">
+          Red
+        </button>
+        <button onClick={() => changeSignal('yellow')} type="button">
+          Yellow
+        </button>
+        <button onClick={() => changeSignal('green')} type="button">
+          Green
+        </button>
       </div>
-    );
-  }
-};
-
-TrafficSignal.contextType = CarsContext;
+      <img className="signal" src={renderSignal(color)} alt="" />
+    </div>
+  );
+}
 
 export default TrafficSignal;
